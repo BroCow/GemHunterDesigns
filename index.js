a3f55e50e6cb72bc5a04943b50c45efb484c9b8e
Christopher Cowan <cow18003@byui.edu>
2021-12-28 05:20:09 -0800

    db

--- index.js
+++ index.js
@@ -40,20 +40,20 @@ const app = express();
   app.get('/necklaces', product_controller.getNecklacesList);
 
   // Get request to connect to Heroku database
-  app.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM necklaces');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/necklaces', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+  // app.get('/db', async (req, res) => {
+  //   try {
+  //     const client = await pool.connect();
+  //     const result = await client.query('SELECT * FROM necklaces');
+  //     const results = { 'results': (result) ? result.rows : null};
+  //     res.render('pages/necklaces', results );
+  //     client.release();
+  //   } catch (err) {
+  //     console.error(err);
+  //     res.send("Error " + err);
+  //   }
+  // })
 
-  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
 
 
   // .get('/addItems', async (req, res) => {
@@ -72,19 +72,19 @@ const app = express();
 
   /*** Template DB Connection ***/
   // Get request to connect to Heroku database
-  // .get('/db', async (req, res) => {
-  //   try {
-  //     const client = await pool.connect();
-  //     const result = await client.query('SELECT * FROM necklaces');
-  //     const results = { 'results': (result) ? result.rows : null};
-  //     res.render('pages/db', results );
-  //     client.release();
-  //   } catch (err) {
-  //     console.error(err);
-  //     res.send("Error " + err);
-  //   }
-  // })
-
+  app.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM necklaces');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
   
 
 
