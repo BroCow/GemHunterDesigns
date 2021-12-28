34102303a820d0bd5bb1aefdf49b49090583a351
Christopher Cowan <cow18003@byui.edu>
2021-12-28 13:04:04 -0800

    routers

--- public/js/manage.js
+++ public/js/manage.js
@@ -4,19 +4,9 @@ const express = require('express');
 const router = express.Router();
 
 // Manage Necklaces page
-router.get('/necklaces', async (req, res) => {
-    try {
-      const client = await pool.connect();
-      const result = await client.query('SELECT * FROM necklaces');
-      const results = { 'results': (result) ? result.rows : null};
-      
-      res.render('pages/necklaces', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+router.get('/necklaces', function(req, res){
+    res.send('Add, delete, and modify necklaces here...');
+});
 
 // Manage Bracelets page
 router.get('/bracelets', function(req, res){
--- public/js/product.js
+++ public/js/product.js
@@ -4,9 +4,19 @@ const express = require('express');
 const router = express.Router();
 
 // Show Necklaces page
-router.get('/necklaces', function(req, res){
-    res.send('View available necklaces here...');
-});
+router.get('/necklaces', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM necklaces');
+      const results = { 'results': (result) ? result.rows : null};
+      
+      res.render('pages/necklaces', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 // Show Bracelets page
 router.get('/bracelets', function(req, res){
