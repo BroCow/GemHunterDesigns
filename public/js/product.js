290c61d6c334d7088507d3a3b74df623f83d3635
Christopher Cowan <cow18003@byui.edu>
2021-12-28 13:08:49 -0800

    routers

--- public/js/product.js
+++ public/js/product.js
@@ -3,6 +3,15 @@
 const express = require('express');
 const router = express.Router();
 
+// Database connection
+const { Pool } = require('pg');
+const connectionString = process.env.DATABASE_URL || "postgres://ilnhpatpqlcdtw:ff9228630d574a88bbebe01ad05d3d5bfb7810ea7d2bba09668ab7ba6980794a@ec2-50-19-160-40.compute-1.amazonaws.com:5432/d40kt7nl9qi54?ssl=true";
+const pool = new Pool({connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 // Show Necklaces page
 router.get('/necklaces', async (req, res) => {
     try {
