28c0565f61231ccea71e57ebc3cba1e7655e56ce
Christopher Cowan <cow18003@byui.edu>
2021-12-29 17:10:30 -0800

    mvc

--- public/js/home_view.js
+++ public/js/home_view.js
@@ -1,9 +1,10 @@
 const app = require('index.js');
+const router = require('routers/product.js');
 
 const productLinks_array = document.getElementsByClassName('home-info_item');
 
 productLinks_array[1].addEventListener('click', function(){
-    app.get('/product/necklaces');
+    router.get('/necklaces');
 })
 
 
