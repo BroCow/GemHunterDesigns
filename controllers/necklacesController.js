04de96660339508d8184b90e38a9f4eeef27f866
CKCowan <soundcommchris@gmail.com>
2022-07-21 15:07:33 -0700

    function

--- controllers/necklacesController.js
+++ controllers/necklacesController.js
@@ -3,22 +3,22 @@ const necklaces_model = require('../models/necklaces_model');
 
 
 // Display list of all necklaces.
-// function control_getAllNecklaces(req, res){
-//     console.log("Getting all necklaces...");
-    
-//     necklaces_model.model_getAllNecklaces(function(error, results){
-//         // res.json(result);
-//         res.render('pages/necklaces', results );
-//     });
-// }
-
 function control_getAllNecklaces(req, res){
     console.log("Getting all necklaces...");
-    let req = necklaces_model.model_getAllNecklaces(results);
-    // necklaces_model.model_getAllNecklaces(function(error, results){
+    
+    necklaces_model.model_getAllNecklaces(function(error, results){
         // res.json(result);
-    res.render('pages/necklaces', req );
+        res.render('pages/necklaces', results );
+    });
 }
+
+// function control_getAllNecklaces(req, res){
+//     console.log("Getting all necklaces...");
+//     let req = necklaces_model.model_getAllNecklaces(results);
+//     // necklaces_model.model_getAllNecklaces(function(error, results){
+//         // res.json(result);
+//     res.render('pages/necklaces', req );
+// }
 
 
 // function displayNecklaces(req, res){
