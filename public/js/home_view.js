3a5459f7d6a44728ac1f049a8cc19587271e8722
CKCowan <soundcommchris@gmail.com>
2022-07-21 15:50:17 -0700

    link array

--- public/js/home_view.js
+++ public/js/home_view.js
@@ -2,30 +2,30 @@
 
 const productLinks_array = document.getElementsByClassName('home-info_item');
 
-const braceletsLink = document.getElementById("bracelets-link");
-const necklacesLink = document.getElementById("necklaces-link");
-const earringsLink = document.getElementById("earrings-link");
+// const braceletsLink = document.getElementById("bracelets-link");
+// const necklacesLink = document.getElementById("necklaces-link");
+// const earringsLink = document.getElementById("earrings-link");
 
-const productPaths_array = [braceletsLink, necklacesLink, earringsLink]
+// const productPaths_array = [braceletsLink, necklacesLink, earringsLink]
 
-for(i = 0, i < productLinks_array.length; i++;){
-    productLinks_array[i].addEventListener('click', function(){
-        console.log(productPaths_array[i]);
-        location.href = `/product/${productPaths_array[i]}`;
-    })
-}
+// for(i = 0, i < productLinks_array.length; i++;){
+//     productLinks_array[i].addEventListener('click', function(){
+//         console.log(productPaths_array[i]);
+//         location.href = `/product/${productPaths_array[i]}`;
+//     })
+// }
 
-// productLinks_array[0].addEventListener('click', function(){
-//     location.href = '/product/bracelets';
-// })
+productLinks_array[0].addEventListener('click', function(){
+    location.href = '/product/bracelets';
+})
 
-// productLinks_array[1].addEventListener('click', function(){
-//     location.href = '/product/necklaces';
-// })
+productLinks_array[1].addEventListener('click', function(){
+    location.href = '/product/necklaces';
+})
 
-// productLinks_array[1].addEventListener('click', function(){
-//     location.href = '/product/necklaces';
-// })
+productLinks_array[2].addEventListener('click', function(){
+    location.href = '/product/earrings';
+})
 
 
 
