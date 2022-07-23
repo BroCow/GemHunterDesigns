52c6ffdb62fe96fa729d444b09525cab529245c7
CKCowan <soundcommchris@gmail.com>
2022-07-23 12:34:48 -0700

    manage

--- controllers/manageProductController.js
+++ controllers/manageProductController.js
@@ -17,6 +17,15 @@ function control_insertNecklace(req, res){
         length: length
     };
 
+    // Database connection
+    const { Pool } = require('pg');
+    const connectionString = process.env.DATABASE_URL || "postgres://ilnhpatpqlcdtw:ff9228630d574a88bbebe01ad05d3d5bfb7810ea7d2bba09668ab7ba6980794a@ec2-50-19-160-40.compute-1.amazonaws.com:5432/d40kt7nl9qi54?ssl=true";
+    const pool = new Pool({connectionString: connectionString,
+    ssl: {
+        rejectUnauthorized: false
+    }
+    }); 
+
     let sql = `INSERT INTO Necklaces (description, length, price) VALUES (${necklaceDetails.description}, ${necklaceDetails.length}, ${necklaceDetails.price})`;
     pool.query(sql, function(err, db_result){
         if(err){
