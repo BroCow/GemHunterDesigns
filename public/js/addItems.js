ceeb437c9c5502267b83876957c12912183e5736
CKCowan <soundcommchris@gmail.com>
2022-07-23 07:42:58 -0700

    manage

--- public/js/addItems.js
+++ public/js/addItems.js
@@ -1,25 +1,32 @@
 console.log('js linked');
 
+const insertNecklaceSubmit = document.getElementById('insertNecklaceSubmit');
+
+insertNecklaceSubmit.addEventListener('click', function(){
+    location.href = '/manage/insertNecklace';
+});
+
+
 
 // const insertNecklace = document.getElementById('insertNecklace');
-function appPost_Necklace(){
-    console.log('Insert button clicked');
-    window.location.href = 'https://fast-bayou-70960.herokuapp.com/insertNecklace';
-}
+// function appPost_Necklace(){
+//     console.log('Insert button clicked');
+//     window.location.href = 'https://fast-bayou-70960.herokuapp.com/insertNecklace';
+// }
 // insertNecklace.addEventListener('click', appPost_Necklace);
 
-function insertData(description, price, length){
-    // let description = document.getElementById('description').value;
-    // let price = document.getElementById('price').value;
-    // let length = document.getElementById('length').value;
+// function insertData(description, price, length){
+//     // let description = document.getElementById('description').value;
+//     // let price = document.getElementById('price').value;
+//     // let length = document.getElementById('length').value;
 
-    // let insertDataArray = ["description", "price", "length"];
-    let insertDataArray = [description, length, price];
+//     // let insertDataArray = ["description", "price", "length"];
+//     let insertDataArray = [description, length, price];
 
-    return insertDataArray;
-}
+//     return insertDataArray;
+// }
 
 
-module.exports = {
-    insertData: insertData
-}
+// module.exports = {
+//     insertData: insertData
+// }
