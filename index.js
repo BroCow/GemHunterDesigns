149a53bf9af3b1293362bf76cffba462c3bd8536
CKCowan <soundcommchris@gmail.com>
2022-07-23 08:51:39 -0700

    manage

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 //  Require routers
 const product = require('./routers/product');
-// const manage = require('./routers/manage');
+const manage = require('./routers/manage');
 
 
 // Database connection
