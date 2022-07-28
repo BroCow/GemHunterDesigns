8687638af6a1c83a9a36d7e1090db8493c3d4ac9
CKCowan <soundcommchris@gmail.com>
2022-07-27 17:28:36 -0700

    button

--- public/js/huntGems.js
+++ public/js/huntGems.js
@@ -4,20 +4,20 @@ const productLinks_array = document.getElementsByClassName('shop_item');
 
 productLinks_array[0].addEventListener('click', function(){
     //location.href = '/product/bracelets';
-    console.log("Shop necklaces requested");
+    console.log("Shop bracelets requested");
 })
 
 productLinks_array[1].addEventListener('click', function(){
-    location.href = '/product/necklaces';
+    //location.href = '/product/necklaces';
+    console.log("Shop necklaces requested");
 })
 
 productLinks_array[2].addEventListener('click', function(){
-    location.href = '/product/earrings';
+    //location.href = '/product/earrings';
+    console.log("Shop earrings requested");
 })
 
-productLinks_array[3].addEventListener('click', function(){
-    location.href = '/product/manageProduct';
-})
+
 
 
 
